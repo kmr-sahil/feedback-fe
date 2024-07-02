2175f076312f09229a41d7a3808caa5f19f1272b
Sahil <sk2003311@gmail.com>
2024-07-02 13:50:45 +0530

    added loader

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,30 @@
-"use client"
+"use client";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function DashboardPage() {
+  const [login, setLogin] = useState(false);
+  const [responses, setResponses] = useState([]);
+
   useEffect(() => {
     const fetch = async () => {
       try {
         const response = await axios.get("http://localhost:8080/v1/responses", {
           withCredentials: true,
         });
+        setResponses(response.data);
         console.log(response);
-      } catch (error) {
+      } catch (error: any) {
+        if (error.response && error.response.status === 401) {
+          setLogin(true);
+        }
         console.log(error);
       }
     };
 
-    fetch()
+    fetch();
   }, []);
-  return <div>DashboardPage</div>;
+  return <div>hii</div>;
 }
 
 export default DashboardPage;
