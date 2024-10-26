79abc8168f797e25c43fcab9cb35aa42c5015191
Sahil Kumar <sk2003311@gmail.com>
2024-10-26 22:33:04 +0530

    fixed search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 interface Company {
   projectId: string;
@@ -24,6 +25,7 @@ interface Company {
 }
 
 export default function CompanySearch() {
+  const router = useRouter()
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [location, setLocation] = useState<string>("");
@@ -111,11 +113,12 @@ export default function CompanySearch() {
       <div className="space-y-4">
         {filteredCompanies.map((company) => (
           <div
+          onClick={() => router.push(`review/${company.website}`)}
             key={company.projectId}
             className="flex items-center space-x-4 p-4 border rounded-lg shadow-sm"
           >
-            <Image
-              src={company.logoUrl || "/placeholder.svg"}
+            <img
+              src={company?.logoUrl || "/placeholder.svg"}
               alt={`${company.name} logo`}
               width={64}
               height={64}
@@ -137,7 +140,7 @@ export default function CompanySearch() {
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
                 <span className="ml-1 font-semibold">
-                  {company.avgRating.toFixed(1)}
+                  { company.avgRating ? company.avgRating.toFixed(1) : 0}
                 </span>
               </div>
               <a
